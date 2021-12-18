a1dc52ebc3b60ba15170f5eda3887c79075c1245
fubu1997 <fubu1997@naver.com>
2021-12-19 00:27:54 +0900

    Wod Timer : Stop-Watch

--- wodTimer/JS/stopWatch.js
+++ wodTimer/JS/stopWatch.js
@@ -1,54 +1,110 @@
-let minutes = 0;
-let seconds = 0;
-let tenMilis = 0;
-const appenTens = document.querySelector('.tenMilis');
-const appenSeconds = document.querySelector('.seconds');
-const appenMinutes = document.querySelector('.minutes');
-const buttonStart = document.querySelector('#startBtn');
-const buttonStop = document.querySelector('#stopBtn');
-const buttonReset = document.querySelector('#resetBtn');
-let intervalId;
+const stopFullRecord = document.getElementById('stop-full-record');
+let hour = 00;
+let min = 00;
+let sec = 00;
+let tenMilis = 00;
 
-buttonStart.onclick = function() {
-  clearInterval(intervalId);
-  intervalId = setInterval(operateTimer, 10);
+const hoursText = document.getElementById('hours');
+const minutesText = document.getElementById('minutes');
+const secondsText = document.getElementById('seconds');
+const tenMilisText = document.getElementById('tenMilis');
+
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const resetBtn = document.getElementById('resetBtn');
+const recordBtn = document.getElementById('recordBtn');
+const recordUl = document.getElementById('testRecordList');
+
+let Interval;
+
+//Click START button then show STOP button
+startBtn.addEventListener('click', function() {
+  clearInterval(Interval);
+  Interval = setInterval(startTimer, 10);
+  stopBtn.classList.remove('hidden');
+  startBtn.classList.add('hidden');
+});
+
+stopBtn.onclick = function() {
+  clearInterval(Interval);
+  createRecord();
+  stopBtn.classList.add('hidden');
+  startBtn.classList.remove('hidden');
+};
+
+recordBtn.onclick = function() {
+  createRecord();
 }
 
-buttonStop.onclick = function() {
-  clearInterval(intervalId);
+resetBtn.onclick = function() {
+  hour = 00;
+  hoursText.innerHTML = "00";
+  min = 00;
+  minutesText.innerHTML = "00";
+  sec = 00;
+  secondsText.innerHTML = "00";
+  tenMilis = 00;
+  tenMilisText.innerHTML = "00";
+  recordUl.innerHTML = "";
+  clearInterval(Interval);
+};
+
+function createRecord() {
+  const node = document.createElement("li");
+  const record = stopFullRecord.textContent;
+  const textnode = document.createTextNode(record);
+  node.appendChild(textnode);
+  recordUl.appendChild(node);
 }
 
-buttonReset.onclick = function() {
-  clearInterval(intervalId);
-  tenMilis = 0; seconds = 0; minutes = 0;
-  appenTens.textContent = "00";
-  appenSeconds.textContent = "00"
-  appenMinutes.textContent = "00"
-}
-
-function operateTimer() {
+function startTimer() {
   tenMilis++;
-  appenTens.textContent = tenMilis > 9 ? tenMilis : '0' + tenMilis
-  if(tenMilis > 99) {
-    seconds++;
-    appenSeconds.textContent = seconds > 9 ? seconds : '0' + seconds
-    tenMilis = 0
-    appenTens.textContent = "00"
+  if(tenMilis < 9) {
+    tenMilisText.innerHTML = "0" + tenMilis;
   }
-  if(seconds > 59) {
-    minutes++;
-    appenMinutes.textContent = minutes > 9 ? minutes : '0' + minutes
-    seconds = 0;
-    appenSeconds.textContent = "00"
+  if(tenMilis > 9) {
+    tenMilisText.innerHTML = tenMilis;
+  }
+  if(tenMilis > 99) {
+    sec++;
+    secondsText.innerHTML = "0" + sec;
+    tenMilis = 0;
+    tenMilisText.innerHTML = "0" + tenMilis;
+  }
+  if(sec < 9) {
+    secondsText.innerHTML = "0" + sec;
+  }
+  if(sec > 9) {
+    secondsText.innerHTML = sec;
+  }
+  if(sec > 59) {
+    min++;
+    minutesText.innerHTML = "0" + min;
+    sec = 0;
+    secondsText.innerHTML = "0" + sec;
+    tenMilis = 0;
+    tenMilisText.innerHTML = "0" + tenMilis;
+  }
+  if(min < 9) {
+    minutesText.innerHTML = "0" + min;
+  }
+  if(min > 9) {
+    minutesText.innerHTML = min;
+  }
+  if(min > 59) {
+    hour++
+    hoursText.innerHTML = "0" + hour;
+    min++;
+    minutesText.innerHTML = "0" + min;
+    sec = 0;
+    secondsText.innerHTML = "0" + sec;
+    tenMilis = 0;
+    tenMilisText.innerHTML = "0" + tenMilis;
+  }
+  if(hour < 9) {
+    hoursText.innerHTML = "0" + hour;
+  }
+  if(hour > 9) {
+    hoursText.innerHTML = hour;
   }
 }
-
-
-// 일정한 시간 간격에 따라 함수를 반복 실행
-// setInterval(반복실행할함수, 10)
-
-// 정해진 시간이 지나면 함수 실행
-// setTimeout(실행할함수, 1000)
-
-// setInterval 메소드 호출되어 반복 실행할 함수 타이머를 들고 후 타이머는 0이 아닌 숫자를 반환
-// clearInterval의 메소드에 전달하면 해당 타이머의 반복실행이 취소된다.
